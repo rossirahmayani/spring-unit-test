1d02874f057c1a7f196b2503b16d105df3d91796
rossirahmayani <rossi.rahmayani.rr@gmail.com>
2020-02-20 06:45:25 +0700

    'test'

--- src/main/java/com/rossi/springunittest/service/Mt940Service.java
+++ src/main/java/com/rossi/springunittest/service/Mt940Service.java
@@ -13,42 +13,45 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @Service
 @Slf4j
 public class Mt940Service {
 
     public DataResponse parseMt940 (String string) {
+        log.info("Start parsing");
         MT940 mt940 = MT940.parse(string);
         log.info(mt940.toJson());
+
+        log.info("Get initial balance");
         BigDecimal initialBalance = mt940.getField60F().amount();
-        BigDecimal prevBalance = mt940.getField60F().amount();
+
+        log.info("Create mutations");
+        BigDecimal prevBalance = initialBalance;
         List<Mutation> mutations = new ArrayList<>();
-        Integer mutationSize = mt940.getField61().size();
+        int mutationSize = mt940.getField61().size();
         for (int idx = 0; idx < mutationSize; idx++){
             Mutation mutation = createMutation(prevBalance, mt940.getField86().get(idx), mt940.getField61().get(idx));
             prevBalance = mutation.getLastMutationBalance();
             mutations.add(mutation);
         }
 
+        log.info("Get last balance");
         BigDecimal lastBalance = Optional.of(mt940.getField62F().amount())
                 .filter(s -> s.equals(mutations.get(mutationSize-1).getLastMutationBalance()))
                 .map(b -> mutations.get(mutationSize-1).getLastMutationBalance()).orElse(null);
+        mutations.forEach(mutation -> log.info(mutation.toString()));
 
+        log.info("Create response");
         MutationResponse data = MutationResponse.builder()
                 .initialBalance(initialBalance)
                 .lastBalance(lastBalance)
-                .mutations(mutations)
-                .build();
-
+                .mutations(mutations).build();
         DataResponse response = new DataResponse();
         response.setResponseCode("0000");
         response.setResponseMsg("Success");
         response.setResponseData(data);
         return response;
-
     }
 
     private Mutation createMutation (BigDecimal prevBalance, Field86 field86, Field61 field61) {
